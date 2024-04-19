c3096dfc40345adc7d6ffee9ae2fc791daa98656
gokul-ravi-16 <154631237+gokul-ravi-16@users.noreply.github.com>
2024-04-19 12:48:53 -0400

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
  
 const app = express();
 app.use(cors({
- origin : [""],
+ origin : ["https://healhubweb.vercel.app/"],
  methods : ["POST","GET"],
  credentials : true
 }
